a9f536a6600425e5de3e6a7ca47db939d5dafd90
Antoine Thomas <antoine.thomas@navya.tech>
2020-03-05 19:26:08 +0100

    Moving pipe

--- app/ui/world.py
+++ app/ui/world.py
@@ -15,12 +15,19 @@ class World(arcade.Window):
     def on_draw(self):
         scale = self.width / self.background.width
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
-        tuyau = arcade.Sprite("../image/pipe.png", 0.5)
-        self.center_x -=1
-        tuyau.center_x = self.center_x
-        tuyau.center_y=300
-        tuyau.draw()
+        self.tuyau = arcade.Sprite("../image/pipe.png", 0.3)
 
+        self.center_x -= 2
+        if self.center_x == 0:
+            self.center_x = 1000
+        self.tuyau.center_x = self.center_x
+        self.tuyau.center_y=200
+        self.tuyau.draw()
+
+class pipeHandler(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.tuyau = arcade.Sprite("../image/pipe.png", 0.3)
 
 
 World(SCREEN_WIDTH,SCREEN_HEIGHT)
