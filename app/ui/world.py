705d079fd6fe605ffcef4feaf1c12cc78568c242
Antoine Thomas <antoine.thomas@navya.tech>
2020-03-10 14:28:43 +0100

    Flappytation et boolean sur collision

--- app/ui/world.py
+++ app/ui/world.py
@@ -11,61 +11,56 @@ class World(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.background = arcade.load_texture("../image/background.png")
-        self.center_x =1000
+
 
         self.bird_list = None
         self.wall_list = None
+        self.player = None
 
-        self.bird = None
+        self.player = arcade.Sprite("../image/bird.png", 0.25)
 
-    def setup(self):
-        """ Setup the game at the beginning"""
-        # Create the Sprite lists
-        self.bird_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
-
-        self.bird = arcade.Sprite("../image/bird.png", 0.25)
-        self.bird.center_x = 100
-        self.bird.center_y = 100
-        self.bird_list.append(self.bird)
-
-        for x in range(0, 1250, 64):
-            wall = arcade.Sprite("../image/ground.png", 0.3)
-            wall.center_x = x
-            wall.center_y = 32
-            self.wall_list.append(wall)
-        # Create the 'physics engine'
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
-                                                             self.wall_list,
-                                                             GRAVITY)
-    def on_draw(self):
-        arcade.start_render()
-        arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
         self.tuyau = arcade.Sprite("../image/pipe.png", 0.3)
 
+        self.player.center_x = 150
+
+        self.center_x = 1000
+
+    def setup(self):
+        pass
+
+    def on_draw(self  ):
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
         self.center_x -= 2
         if self.center_x <= 0:
             self.center_x = 1000
         self.tuyau.center_x = self.center_x
         self.tuyau.center_y=200
-        self.tuyau.draw()
-        self.bird.draw()
 
+        if self.player.center_y >0 :
+            self.player.center_y -=  2
+        self.tuyau.draw()
+        self.player.draw()
+
+    def on_update(self, delta_time):
+        hit_list =arcade.check_for_collision(self.player, self.tuyau)
+
+        print(hit_list)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            if self.physics_engine.can_jump():
-                self.bird.change_y = PLAYER_JUMP_SPEED
+        if key == arcade.key.SPACE:
+            if self.player.center_y < SCREEN_HEIGHT - 40:
 
-class pipeHandler(arcade.Sprite):
-    def __init__(self, x, y):
-        super().__init__(x, y)
-        self.tuyau = arcade.Sprite("../image/pipe.png", 0.3)
+                self.player.center_y +=80
 
 
-World(SCREEN_WIDTH,SCREEN_HEIGHT)
-World.setup()
-arcade.run()
 
+
+def main():
+    World(SCREEN_WIDTH,SCREEN_HEIGHT)
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
